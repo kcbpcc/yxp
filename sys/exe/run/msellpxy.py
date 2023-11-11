6fe9b61f351e2d45c5c2d9426c9cb7619387a37f
PreciseXceleratedYield <148702748+PreciseXceleratedYieldPXY@users.noreply.github.com>
2023-11-12 00:48:00 +0530

    Add files via upload

--- sys/exe/run/msellpxy.py
+++ sys/exe/run/msellpxy.py
@@ -6,12 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
+# Set the path to the GeckoDriver executable
+geckodriver_path = '/home/userland/local/bin/geckodriver'  # Replace with the actual path
+
 # Set up Firefox options for running in headless mode
 firefox_options = Options()
 firefox_options.headless = True
 
-# Create a WebDriver instance with the specified Firefox options
-driver = webdriver.Firefox(options=firefox_options)
+# Create a WebDriver instance with the specified Firefox options and executable path
+driver = webdriver.Firefox(options=firefox_options, executable_path=geckodriver_path)
+
+# Rest of your code remains the same...
+
 
 # Rest of your code remains the same
 url = 'https://scanners.streak.tech/scanner/minuspxy'
