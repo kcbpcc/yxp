662c8197474843fa876523406a5b95434c3f8347
PreciseXceleratedYield <148702748+PreciseXceleratedYieldPXY@users.noreply.github.com>
2023-11-11 17:17:31 +0530

    Add files via upload

--- sys/exe/run/msellpxy.py
+++ sys/exe/run/msellpxy.py
@@ -17,6 +17,8 @@ from ynfndpxy import calculate_decision
 from mktpxy import mktpxy
 import time
 from byhopxy import get
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 # Set up Chrome options for running in headless mode
 chrome_options = Options()
@@ -25,9 +27,7 @@ chrome_options.add_argument('--disable-gpu')  # Disable GPU for headless mode
 
 # Specify the path to your Chromium WebDriver executable
 chromium_driver_path = '/usr/bin/chromedriver'  # Replace this with the actual path
-driver = webdriver.Chrome(options=chrome_options, service=Service(chromium_driver_path)
-
-
+driver = webdriver.Chrome(options=chrome_options, service=Service(chromium_driver_path))
 
 # Close the browser window
 driver.quit()
