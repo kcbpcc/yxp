c70200eef338183209e23a5a1c4c48c38233eff5
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-12 19:49:55 +0530

    Delete sys/exe/run/1.py

--- sys/exe/run/1.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-chrome_options = webdriver.ChromeOptions()
-# Add options as needed
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
