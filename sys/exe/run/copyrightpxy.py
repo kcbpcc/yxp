b48a3bbad139cf9ca669bc8a5e9be9699fab5088
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-13 12:03:26 +0530

    Update copyrightpxy.py

--- sys/exe/run/copyrightpxy.py
+++ sys/exe/run/copyrightpxy.py
@@ -4,15 +4,11 @@ import textwrap
 
 # Copyright Notice
 copyright_notice = (
-    "The 'PXY®' trading tool and its content "
-    "are protected by copyright laws and "
-    "international treaties. All rights "
-    "reserved by 'PXY®. Unauthorized use, "
-    "reproduction, and distribution are "
-    "strictly prohibited. Infringement may "
-    "lead to legal action and financial "
-    "penalties. 'PXY® is committed to "
-    "protecting its intellectual property."
+    "The 'PXY® trading tool and its content are protected by copyright laws and international treaties."
+    "All rights reserved by 'PXY®. Unauthorized use, reproduction, and distribution are strictly prohibited."
+    "Infringement may lead to legal action and financial penalties."
+    "PXY® is committed to protecting its intellectual property."
+
 )
 
 # Set the desired width
