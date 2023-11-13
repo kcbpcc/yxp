c879e5ae51819b389b6a70e2c0758e9510712ed1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-13 12:05:21 +0530

    Update copyrightpxy.py

--- sys/exe/run/copyrightpxy.py
+++ sys/exe/run/copyrightpxy.py
@@ -4,7 +4,7 @@ import textwrap
 
 # Copyright Notice
 copyright_notice = (
-    "The 'PXY® trading tool and its content are protected by copyright laws and international treaties."
+    "The PXY® trading tool and its content are protected by copyright laws and international treaties."
     "All rights reserved by 'PXY®. Unauthorized use, reproduction, and distribution are strictly prohibited."
     "Infringement may lead to legal action and financial penalties."
     "PXY® is committed to protecting its intellectual property."
