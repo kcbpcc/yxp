144867bdd66d31d2712f5870fe63441e29ed2326
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 11:43:41 +0530

    Update pluspxy.py

--- sys/exe/run/pluspxy.py
+++ sys/exe/run/pluspxy.py
@@ -141,7 +141,7 @@ try:
     import mktpxy
     import importlib
     from daypxy import get_nse_action
-    file_path = "filePnL.csv"
+    csv_file_path = "filePnL.csv"
     total_profit_main = process_csv(csv_file_path)
     mktpxy = mktpxy.get_market_check('^NSEI')
     SILVER = "\033[97m"
