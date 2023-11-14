42aa3817187dd331515c45e7c9c192bf2f7276e3
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 14:11:32 +0530

    Update mktpxy.py

--- sys/exe/run/mktpxy.py
+++ sys/exe/run/mktpxy.py
@@ -59,19 +59,19 @@ def get_market_check(symbol):
     # Determine the market check based on the candle colors and use rich.print to format output
     if current_color == 'Bear' and last_closed_color == 'Bear':
         mktpxy = 'Bear'
-        pktpxy =  '🐻🔴🔴🔴'
+        pktpxy =  '🐻🔴🔴'
         console.print("🐻🔴🔴🔴 [bold]Bearish sentiment![/bold] 🍯💰", style=bear_style)
     elif current_color == 'Bull' and last_closed_color == 'Bull':
         mktpxy = 'Bull'
-        pktpxy =  '🐂🟢🟢🟢'
+        pktpxy =  '🐂🟢🟢'
         console.print("🐂🟢🟢🟢 [bold]Bullish sentiment![/bold] 💪💰", style=bull_style)
     elif current_color == 'Bear' and last_closed_color == 'Bull':
         mktpxy = 'Sell'
-        pktpxy =  '🛒🔴🛬⤵️'
+        pktpxy =  '🔴🛬⤵️'
         console.print("🛒🔴🛬⤵️ [bold]Time to sell![/bold] 📉💰", style=sell_style) 
     elif current_color == 'Bull' and last_closed_color == 'Bear':
         mktpxy = 'Buy'
-        pktpxy =  '🚀🟢🛫⤴️'
+        pktpxy =  '🟢🛫⤴️'
         console.print("🚀🟢🛫⤴️ [bold]Time to buy![/bold] 🌠💰", style=buy_style)
     else:
         mktpxy = 'None'
