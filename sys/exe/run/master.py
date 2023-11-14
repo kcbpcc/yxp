5ca7b7e5404e8f173662df22e8e49297e006e1b4
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 22:42:03 +0530

    Update master.py

--- sys/exe/run/master.py
+++ sys/exe/run/master.py
@@ -282,7 +282,7 @@ try:
     print(f"DataFrame has been saved to {lstchk_file}")
     # Create a copy of 'filtered_df' and select specific columns
     pxy_df = filtered_df.copy()[['source','product', 'qty','average_price', 'close', 'ltp', 'open', 'high','low', 'key','dPnL%','PnL','PnL%_H', 'PnL%']]
-    pxy_df['Pr'] = (1+max(0.2, (0 + (NIFTY['strength'] * 2).round(1).max())))
+    pxy_df['Pr'] = max(1, 1 + max(0.2, (0 + (NIFTY['strength'] * 2).round(1).max())))
     pxy_df['Xl'] = Xlratd
     pxy_df['Yi'] = Yi
     pxy_df['PXY'] = np.where(mktpxy == 'Bear', Precise, np.where((mktpxy == 'Buy') | (mktpxy == 'Bull'), Yield, Xlratd))
