7713592a9b880a148592e37c3b77473a33f0ce93
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 10:23:03 +0530

    Update prftpxy.py

--- sys/exe/run/prftpxy.py
+++ sys/exe/run/prftpxy.py
@@ -2,77 +2,69 @@ import csv
 from rich import print
 from rich.table import Table
 
-def process_csv(csv_file_path):
-    # Set the overall table width
-    table_width = 45
+# Replace this with your CSV file path
+csv_file_path = "filePnL.csv"
 
-    # Create a table to display the selected columns with custom headers
-    table = Table(show_header=True, header_style="bold cyan", min_width=table_width)
-    table.add_column("PH")
-    table.add_column("CM")
-    table.add_column("Key")
-    table.add_column("PXY")
-    table.add_column("PnL%")
-    table.add_column("PnL")
+# Set the overall table width
+table_width = 45
 
-    # Initialize the total profit variable
-    total_profit = 0
+# Create a table to display the selected columns with custom headers
+table = Table(show_header=True, header_style="bold cyan", min_width=table_width)
+table.add_column("PH")
+table.add_column("CM")
+table.add_column("Key")
+table.add_column("PXY")
+table.add_column("PnL%")
+table.add_column("PnL")
 
-    try:
-        # Open the CSV file for reading
-        with open(csv_file_path, newline='') as csvfile:
-            # Create a CSV reader
-            csvreader = csv.reader(csvfile)
+# Initialize the total profit variable
+total_profit = 0
 
-            # Skip the header row
-            header_row = next(csvreader)
+try:
+    # Open the CSV file for reading
+    with open(csv_file_path, newline='') as csvfile:
+        # Create a CSV reader
+        csvreader = csv.reader(csvfile)
 
-            # Rename the columns in the table
-            table.field_names = ["P/H" if col.lower() == "PH" else ("C/M" if col.lower() == "CM" else col) for col in header_row]
+        # Skip the header row
+        header_row = next(csvreader)
 
-            # Iterate over each row in the CSV file and add it to the table
-            for row in csvreader:
-                # Adjust column indices to match your CSV file structure
-                PH, CM, key, qty, avg, close, ltp, open_, high, low, dpnl_percentage, pxy, pnl_percentage, pnl = row
+        # Rename the columns in the table
+        table.field_names = ["P/H" if col.lower() == "PH" else ("C/M" if col.lower() == "CM" else col) for col in header_row]
 
-                # Remove "NSE:" or "BSE:" prefix from the "Key" column
-                key = key.replace("NSE:", "").replace("BSE:", "")
+        # Iterate over each row in the CSV file and add it to the table
+        for row in csvreader:
+            # Adjust column indices to match your CSV file structure
+            PH, CM, key, qty, avg, close, ltp, open, high, low, dpnl_percentage, pxy, pnl_percentage, pnl = row
 
-                # Convert numerical values to strings and round them to two decimal places
-                pxy = str(round(float(pxy), 1))
-                pnl_percentage = str(round(float(pnl_percentage), 1))
-                pnl = str(round(float(pnl)))
+            # Remove "NSE:" or "BSE:" prefix from the "Key" column
+            key = key.replace("NSE:", "").replace("BSE:", "")
 
-                # Map CM values
-                CM_mapping = {"CNC": "C", "MIS": "M"}
-                mapped_CM = CM_mapping.get(CM.upper(), CM)
+            # Convert numerical values to strings and round them to two decimal places
+            pxy = str(round(float(pxy), 1))
+            pnl_percentage = str(round(float(pnl_percentage), 1))
+            pnl = str(round(float(pnl)))
 
-                # Map PH values
-                PH_mapping = {"positions": "P", "holdings": "H"}
-                mapped_PH = PH_mapping.get(PH.lower(), PH)
+            # Map CM values
+            CM_mapping = {"CNC": "C", "MIS": "M"}
+            mapped_CM = CM_mapping.get(CM.upper(), CM)
 
-                # Accumulate the total profit
-                total_profit += float(pnl)
+            # Map PH values
+            PH_mapping = {"positions": "P", "holdings": "H"}
+            mapped_PH = PH_mapping.get(PH.lower(), PH)
 
-                # Add the row to the table
-                table.add_row(mapped_PH, mapped_CM, key, pxy, pnl_percentage, pnl)
+            # Accumulate the total profit
+            total_profit += float(pnl)
 
-        # Print the table with the updated column names
-        print(table)
+            # Add the row to the table
+            table.add_row(mapped_PH, mapped_CM, key, pxy, pnl_percentage, pnl)
 
-    except FileNotFoundError:
-        print("File not found!")
-
-    # Return the total profit
-    return total_profit
-
-if __name__ == "__main__":
-    # Replace this with your CSV file path
-    csv_file_path = "filePnL.csv"
-
-    # Call the function to get the total profit
-    total_profit = process_csv(csv_file_path)
+    # Print the table with the updated column names
+    print(table)
 
     # Print the total profit in INR (₹) format rounded to two decimal places
     total_profit = round(total_profit, 2)
     print(f"Total Profit: ₹{total_profit:.2f}")
+
+except FileNotFoundError:
+    print("File not found!")
