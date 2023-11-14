7ceef9cc7a1140c2bf33afc688fdb35e050b2d0c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 14:50:11 +0530

    Update pluspxy.py

--- sys/exe/run/pluspxy.py
+++ sys/exe/run/pluspxy.py
@@ -431,8 +431,8 @@ try:
         print(right_aligned_format.format(f"Precise:{BRIGHT_GREEN if Precise > 1.4 else BRIGHT_RED}{round(Precise, 2)}{RESET}"))
         print(left_aligned_format.format(f"@PnL:{BRIGHT_GREEN if total_PnL >= 0 else BRIGHT_RED}{round(total_PnL, 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"Xlratd:{BRIGHT_GREEN if Xlratd > 2.4 else BRIGHT_RED}{round(Xlratd, 2)}{RESET}"))
-        print(left_aligned_format.format(f"@PnL%:{BRIGHT_GREEN if total_PnL_percentage >= 0 else BRIGHT_RED}{round(total_PnL_percentage, 2)}{RESET}"), end="")
-        print(right_aligned_format.format(f"pktpxy:{pktpxy}{RESET}"))
+        print(left_aligned_format.format(f"{pktpxy}{RESET}"), end="")
+        print(right_aligned_format.format(pktpxy}{RESET}"))
 
 
         print(f'{SILVER}{UNDERLINE}🏛🏛🏛PXY® PreciseXceleratedYield Pvt Ltd™🏛🏛🏛{RESET}')
