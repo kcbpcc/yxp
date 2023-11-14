5a3e266cc1615fd3e98a115fd0c9f1bfd744d25c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-14 10:14:47 +0530

    Update prftpxy.py

--- sys/exe/run/prftpxy.py
+++ sys/exe/run/prftpxy.py
@@ -18,45 +18,44 @@ def process_csv(csv_file_path):
     # Initialize the total profit variable
     total_profit = 0
 
-try:
-    # Open the CSV file for reading
-    with open(csv_file_path, newline='') as csvfile:
-        # Create a CSV reader
-        csvreader = csv.reader(csvfile)
+    try:
+        # Open the CSV file for reading
+        with open(csv_file_path, newline='') as csvfile:
+            # Create a CSV reader
+            csvreader = csv.reader(csvfile)
 
-        # Skip the header row
-        header_row = next(csvreader)
+            # Skip the header row
+            header_row = next(csvreader)
 
-        # Rename the columns in the table
-        table.field_names = ["P/H" if col.lower() == "PH" else ("C/M" if col.lower() == "CM" else col) for col in header_row]
+            # Rename the columns in the table
+            table.field_names = ["P/H" if col.lower() == "PH" else ("C/M" if col.lower() == "CM" else col) for col in header_row]
 
-        # Iterate over each row in the CSV file and add it to the table
-        for row in csvreader:
-            # Adjust column indices to match your CSV file structure
-            PH, CM, key, qty, avg, close, ltp, open_, high, low, dpnl_percentage, pxy, pnl_percentage, pnl = row
+            # Iterate over each row in the CSV file and add it to the table
+            for row in csvreader:
+                # Adjust column indices to match your CSV file structure
+                PH, CM, key, qty, avg, close, ltp, open_, high, low, dpnl_percentage, pxy, pnl_percentage, pnl = row
 
-            # Remove "NSE:" or "BSE:" prefix from the "Key" column
-            key = key.replace("NSE:", "").replace("BSE:", "")
+                # Remove "NSE:" or "BSE:" prefix from the "Key" column
+                key = key.replace("NSE:", "").replace("BSE:", "")
 
-            # Convert numerical values to strings and round them to two decimal places
-            pxy = str(round(float(pxy), 1))
-            pnl_percentage = str(round(float(pnl_percentage), 1))
-            pnl = str(round(float(pnl)))
+                # Convert numerical values to strings and round them to two decimal places
+                pxy = str(round(float(pxy), 1))
+                pnl_percentage = str(round(float(pnl_percentage), 1))
+                pnl = str(round(float(pnl)))
 
-            # Map CM values
-            CM_mapping = {"CNC": "C", "MIS": "M"}
-            mapped_CM = CM_mapping.get(CM.upper(), CM)
+                # Map CM values
+                CM_mapping = {"CNC": "C", "MIS": "M"}
+                mapped_CM = CM_mapping.get(CM.upper(), CM)
 
-            # Map PH values
-            PH_mapping = {"positions": "P", "holdings": "H"}
-            mapped_PH = PH_mapping.get(PH.lower(), PH)
+                # Map PH values
+                PH_mapping = {"positions": "P", "holdings": "H"}
+                mapped_PH = PH_mapping.get(PH.lower(), PH)
 
-            # Accumulate the total profit
-            total_profit += float(pnl)
+                # Accumulate the total profit
+                total_profit += float(pnl)
 
-            # Add the row to the table
-            table.add_row(mapped_PH, mapped_CM, key, pxy, pnl_percentage, pnl)
-
+                # Add the row to the table
+                table.add_row(mapped_PH, mapped_CM, key, pxy, pnl_percentage, pnl)
 
         # Print the table with the updated column names
         print(table)
