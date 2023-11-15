f59b5ccac4d2b0aa1fb252888d85d16011592c1c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-15 12:12:27 +0530

    Update yipxy.py

--- sys/exe/run/yipxy.py
+++ sys/exe/run/yipxy.py
@@ -2,19 +2,18 @@ import datetime
 import pytz
 
 def calculate_Yi():
-    # Define the time zone (Indian Standard Time)
-    ist_timezone = pytz.timezone('Asia/Kolkata')
+    # Define the start time in UTC
+    start_time_utc = datetime.datetime.utcnow().replace(hour=3, minute=30, second=0, microsecond=0, tzinfo=pytz.utc)
+    
+    # Define the end time in UTC
+    end_time_utc = datetime.datetime.utcnow().replace(hour=10, minute=0, second=0, microsecond=0, tzinfo=pytz.utc)
 
-    # Get the current date and time in IST
-    current_datetime_ist = datetime.datetime.now(ist_timezone)
+    # Get the current date and time in UTC
+    current_datetime_utc = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
 
-    # Define the start and end times in IST
-    start_time_ist = datetime.datetime.combine(current_datetime_ist.date(), datetime.time(9, 0)).astimezone(ist_timezone)
-    end_time_ist = datetime.datetime.combine(current_datetime_ist.date(), datetime.time(15, 30)).astimezone(ist_timezone)
-
-    # Calculate Yi value based on the time difference
-    if start_time_ist <= current_datetime_ist <= end_time_ist:
-        total_minutes = (current_datetime_ist - start_time_ist).total_seconds() / 60
+    # Calculate Yi value based on the minute difference in UTC
+    if start_time_utc <= current_datetime_utc <= end_time_utc:
+        total_minutes = (current_datetime_utc - start_time_utc).total_seconds() / 60
         Yi = max(5, round(15 - total_minutes / 30, 1))
         return Yi
     else:
@@ -23,4 +22,4 @@ def calculate_Yi():
 
 # Example usage:
 result = calculate_Yi()
-#print(f"Yi value: {result}")
+print(f"Yi value: {result}")
