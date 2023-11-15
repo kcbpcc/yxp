513e6d5be95f0b0f0352edcc14ce319995f3c807
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-15 14:39:20 +0530

    Update clnpxy.py

--- sys/exe/run/clnpxy.py
+++ sys/exe/run/clnpxy.py
@@ -4,14 +4,14 @@ from datetime import datetime, timedelta
 # Specify the path to your CSV file
 file_path = 'filePnL.csv'
 
-# Function to check if the current time is between 7:00 AM and 9:14 AM IST
+# Function to check if the current time is between 2:00 AM and 3:00 AM UTC
 def is_time_between(start_time, end_time):
-    current_time = datetime.now() + timedelta(hours=5, minutes=30)  # Convert to IST
+    current_time = datetime.utcnow()
     return start_time <= current_time <= end_time
 
-# Specify the time range during which you want to delete contents (7:00 AM to 9:14 AM IST)
-start_time = datetime.now().replace(hour=7, minute=0, second=0, microsecond=0) + timedelta(hours=5, minutes=30)
-end_time = datetime.now().replace(hour=9, minute=14, second=0, microsecond=0) + timedelta(hours=5, minutes=30)
+# Specify the time range during which you want to delete contents (2:00 AM to 3:00 AM UTC)
+start_time = datetime.utcnow().replace(hour=2, minute=0, second=0, microsecond=0)
+end_time = datetime.utcnow().replace(hour=3, minute=0, second=0, microsecond=0)
 
 # Check if the current time is within the specified range
 if is_time_between(start_time, end_time):
@@ -22,4 +22,3 @@ if is_time_between(start_time, end_time):
 
         # Write an empty row to clear the contents
         csv_writer.writerow([])
-
