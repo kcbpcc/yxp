d0b3251dc0d374ecc2a74f52b33f9af23cabb668
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-15 11:36:48 +0530

    Update yipxy.py

--- sys/exe/run/yipxy.py
+++ sys/exe/run/yipxy.py
@@ -12,10 +12,15 @@ def calculate_Yi():
     start_time_ist = datetime.datetime.combine(current_datetime_ist.date(), datetime.time(9, 0)).astimezone(ist_timezone)
     end_time_ist = datetime.datetime.combine(current_datetime_ist.date(), datetime.time(15, 30)).astimezone(ist_timezone)
 
+    print(f"Current IST Time: {current_datetime_ist}")
+    print(f"Start Time IST: {start_time_ist}")
+    print(f"End Time IST: {end_time_ist}")
+
     if start_time_ist <= current_datetime_ist <= end_time_ist:
         # Calculate Yi value based on the time difference
         time_difference = current_datetime_ist - datetime.datetime.combine(current_datetime_ist.date(), datetime.datetime.min.time().replace(tzinfo=ist_timezone))
         minutes_difference = time_difference.total_seconds() / 60
+        print(f"Minutes Difference: {minutes_difference}")
         Yi = max(5, round(15 - minutes_difference / 30, 1))
         return Yi
     else:
@@ -25,10 +30,6 @@ def calculate_Yi():
 # Example usage:
 result = calculate_Yi()
 print(f"Yi value: {result}")
-
-
-
-
 
 
 
