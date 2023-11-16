78d25b69bd61a331b9454b263fe2dbbeac1ca334
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-16 18:52:59 +0530

    Update adptpxy.py

--- sys/exe/run/adptpxy.py
+++ sys/exe/run/adptpxy.py
@@ -251,13 +251,13 @@ try:
         (NIFTY['Day_Change_%'] < 0) & (NIFTY['Open_Change_%'] < 0),
         (NIFTY['Day_Change_%'] > 0) & (NIFTY['Open_Change_%'] < 0)
     ]
-    choices = ['SuperBull', 'Bull', 'DangerBear', 'Bear']
+    choices = ['Super Bull', 'Bull', 'Danger Bear', 'Bear']
     NIFTY['Day Status'] = np.select(NIFTYconditions, choices, default='Bear')
     status_factors = {
-        'SuperBull': 2.8,
+        'Super Bull': 2.8,
         'Bull': 1.4,
         'Bear': 0.7,
-        'DangerBear': 0
+        'Danger Bear': 0
     }
     # Calculate 'Score' for each row based on 'Day Status' and 'status_factors'
     NIFTY['Score'] = NIFTY['Day Status'].map(status_factors).fillna(0)
@@ -444,7 +444,7 @@ try:
         right_aligned_format = "{:>" + str(column_width) + "}"
         print(left_aligned_format.format(f"Day Change%:{BRIGHT_GREEN if NIFTY['Day_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Day_Change_%'][0], 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL {BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{round(total_dPnL, 2)}{RESET}"))
-        print(left_aligned_format.format(f"Day Status:{BRIGHT_GREEN if NIFTY['Day Status'][0] in ('Bull', 'SuperBull') else BRIGHT_RED}{NIFTY['Day Status'][0]}{RESET}"), end="")
+        print(left_aligned_format.format(f"Day Status:{BRIGHT_GREEN if NIFTY['Day Status'][0] in ('Bull', 'Super Bull') else BRIGHT_RED}{NIFTY['Day Status'][0]}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL%:{BRIGHT_GREEN if total_dPnL_percentage > 0 else BRIGHT_RED}{round(total_dPnL_percentage, 2)}{RESET}"))
         print(left_aligned_format.format(f"Day Open%:{BRIGHT_GREEN if NIFTY['Open_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Open_Change_%'][0], 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"Precise:{BRIGHT_GREEN if Precise > 1.4 else BRIGHT_RED}{round(Precise, 2)}{RESET}"))
