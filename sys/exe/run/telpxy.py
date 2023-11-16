79fa461c37170b6b96dedcb3bd19dae731577f27
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-16 12:37:39 +0530

    Update telpxy.py

--- sys/exe/run/telpxy.py
+++ sys/exe/run/telpxy.py
@@ -1,19 +1,34 @@
-# telpxy.py
-
-import telegram
+import asyncio
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 # Move the token, usernames, and message_text definition here
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
 user_usernames = ('-4022487175')
 
-def create_message_text(row, key):
+async def create_message_text(row, key):
     return f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
 
-def send_telegram_message(row, key):
+async def send_telegram_message(row, key):
     try:
-        message_text = create_message_text(row, key)
-        bot = telegram.Bot(token=bot_token)
-        bot.send_message(chat_id=user_usernames, text=message_text)
+        message_text = await create_message_text(row, key)
+        bot = Bot(token=bot_token)
+        await bot.send_message(chat_id=user_usernames, text=message_text)
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
+
+# Usage example
+async def main():
+    # Example data
+    row_data = "Some data"
+    key_data = "BTCUSD"
+
+    # Call the asynchronous function
+    await send_telegram_message(row_data, key_data)
+
+# Run the event loop
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
