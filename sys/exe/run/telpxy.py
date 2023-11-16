c6877946dbd7534652a6a6e844e9b5d0808e2b43
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-16 13:36:39 +0530

    Update telpxy.py

--- sys/exe/run/telpxy.py
+++ sys/exe/run/telpxy.py
@@ -1,20 +1,19 @@
 import asyncio
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+from telegram import Bot
+from telegram import ParseMode
 
 # Move the token, usernames, and message_text definition here
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
 user_usernames = ('-4022487175')
 
 async def create_message_text(row, key):
-    return f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
+    return f"{str(row)} Symbol: {key} [TradingView Chart](https://www.tradingview.com/chart/?symbol={key})"
 
 async def send_telegram_message(row, key):
     try:
         message_text = await create_message_text(row, key)
         bot = Bot(token=bot_token)
-        await bot.send_message(chat_id=user_usernames, text=message_text)
+        bot.send_message(chat_id=user_usernames, text=message_text, parse_mode=ParseMode.MARKDOWN)
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
