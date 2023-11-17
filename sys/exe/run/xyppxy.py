52f1333dae915df7fd5ca77012ce4d47249cc5aa
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 23:10:51 +0530

    Update xyppxy.py

--- sys/exe/run/xyppxy.py
+++ sys/exe/run/xyppxy.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 from timpxy import calculate_timpxy
 from mktpxy import get_market_check
-import math
 
 mktpxy, pktpxy = get_market_check('^NSEI')
+
 # Function to fetch NIFTY data using yfinance
 def fetch_nifty_data():
     nifty_ticker = yf.Ticker('^NSEI')  # NSE NIFTY 50 index symbol
@@ -33,7 +33,6 @@ Xlratd = NIFTY['strength'] * timpxy
 # Calculate Yield
 Yield = timpxy * (-1)
 
-
 # Assuming NIFTY['Day_Change_%'] is a Pandas Series
 _Precise = min(1.3, (NIFTY['weakness']).round(1).max(), -1)
 _Xlratd = (NIFTY['weakness'] * timpxy).clip(None, -1)
@@ -44,7 +43,7 @@ print("Precise:", round(Precise, 1))
 print("Xlratd:", Xlratd.round(1).values)  # Use .values to remove the Datetime index
 print("Yield:", round(Yield, 1))
 
-
 print("_Precise:", round(_Precise, 1))
 print("_Xlratd:", _Xlratd.round(1).values)  # Use .values to remove the Datetime index
 print("_Yield:", round(_Yield, 1))
+
