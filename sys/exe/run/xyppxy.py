21bc62a480dd9ec9fc98c909ce2b01f90dcee34f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 23:18:25 +0530

    Update xyppxy.py

--- sys/exe/run/xyppxy.py
+++ sys/exe/run/xyppxy.py
@@ -33,17 +33,39 @@ Xlratd = NIFTY['strength'] * timpxy
 # Calculate Yield
 Yield = timpxy * (-1)
 
+# Conditions and choices for PXY
+conditions_pxy = [
+    (mktpxy == 'Bull') | (mktpxy == 'Buy'),
+    (mktpxy == 'Sell'),
+    (mktpxy == 'Bear'),
+    (mktpxy == 'Bull')
+]
+choices_pxy = ['Yield', 'Xlratd', 'Precise', 'Yield']
+PXY = np.select(conditions_pxy, choices_pxy)
+
 # Assuming NIFTY['Day_Change_%'] is a Pandas Series
 _Precise = min(1.3, (NIFTY['weakness']).round(1).max(), -1)
 _Xlratd = (NIFTY['weakness'] * timpxy).clip(None, -1)
 _Yield = timpxy * (-1)
 
+# Conditions and choices for _PXY
+_conditions_pxy = [
+    (mktpxy == 'Bear') | (mktpxy == 'Buy'),
+    (mktpxy == 'Sell'),
+    (mktpxy == 'Bear'),
+    (mktpxy == 'Bull')
+]
+_choices_pxy = ['_Yield', '_Xlratd', '_Precise', '_Yield']
+_PXY = np.select(_conditions_pxy, _choices_pxy)
+
 # Print values with one decimal place
 print("Precise:", round(Precise, 1))
-print("Xlratd:", Xlratd.round(1).values)  # Use .values to remove the Datetime index
+print("Xlratd:", Xlratd.round(1))
 print("Yield:", round(Yield, 1))
+print("PXY:", PXY.astype(str).replace('nan', 'NaN').round(1))
 
 print("_Precise:", round(_Precise, 1))
-print("_Xlratd:", _Xlratd.round(1).values)  # Use .values to remove the Datetime index
+print("_Xlratd:", _Xlratd.round(1))
 print("_Yield:", round(_Yield, 1))
+print("_PXY:", _PXY.astype(str).replace('nan', 'NaN').round(1))
 
