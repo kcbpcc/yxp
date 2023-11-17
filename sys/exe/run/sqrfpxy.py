060ad647c54e476ba068b6c1663866a54f63a45a
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 14:45:35 +0530

    Update sqrfpxy.py

--- sys/exe/run/sqrfpxy.py
+++ sys/exe/run/sqrfpxy.py
@@ -14,19 +14,21 @@ def place_mis_orders(positions_df, broker):
         # Place orders for MIS positions
         for index, position in positions_df.iterrows():
             logging.info(f"Placing order for {position['tradingsymbol']}")
-            order_id = broker.order_place(
-                tradingsymbol=position['tradingsymbol'],
-                exchange=position['exchange'],
-                transaction_type='BUY',
-                quantity=int(-1 * position['quantity']),  # Selling the existing position
-                order_type='MARKET',
-                product='MIS',
-                variety='regular'
-            )
-            if order_id:
-                logging.info(f"Order {order_id} placed successfully for {position['tradingsymbol']}")
-            else:
-                logging.error("Order placement failed")
+            # Check if quantity is less than 0 and the product is 'MIS', indicating a buy order for MIS positions
+            if position['quantity'] < 0 and position['product'] == 'MIS':
+                order_id = broker.order_place(
+                    tradingsymbol=position['tradingsymbol'],
+                    exchange=position['exchange'],
+                    transaction_type='BUY',  # Change to BUY for buying positions
+                    quantity=int(-1 * position['quantity']),  # Buying the existing position
+                    order_type='MARKET',
+                    product='MIS',
+                    variety='regular'
+                )
+                if order_id:
+                    logging.info(f"Order {order_id} placed successfully for {position['tradingsymbol']}")
+                else:
+                    logging.error("Order placement failed")
     except Exception as e:
         print(traceback.format_exc())
         logging.error(f"{str(e)} while placing orders")
@@ -49,25 +51,8 @@ result = place_mis_orders(mis_positions, broker)
 
 # Do something with the result if needed
 if result:
-    print("Orders placed successfully.")
+    print("Buy orders placed successfully for MIS positions.")
 else:
-    print("Failed to place orders.")
-
-# Get open MIS positions again for conversion
-mis_positions = pd.DataFrame([pos for pos in broker.positions()['net'] if pos['product'] == 'MIS' and pos['quantity'] > 0])
-
-# Convert MIS to CNC
-for index, position in mis_positions.iterrows():
-    print(f"Symbol: {position['trading_symbol']}, Quantity: {position['quantity']}, Product: {position['product']}")
-    broker.place_order(
-        tradingsymbol=position['trading_symbol'],
-        exchange=position['exchange'],
-        quantity=position['quantity'],
-        transaction_type='CONVERT',
-        order_type='MARKET',
-        product='CNC'
-    )
-
-print("Conversion complete.")
+    print("Failed to place buy orders for MIS positions.")
 
 
