891124b3b943320cb5fbc94629a7b11410130471
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 14:52:14 +0530

    Update sqrfpxy.py

--- sys/exe/run/sqrfpxy.py
+++ sys/exe/run/sqrfpxy.py
@@ -11,24 +11,22 @@ from cnstpxy import dir_path, fileutils, buybuff, max_target
 
 def place_mis_orders(positions_df, broker):
     try:
-        # Place orders for MIS positions
+        # Place orders for MIS positions where quantity is less than 0
         for index, position in positions_df.iterrows():
             logging.info(f"Placing order for {position['tradingsymbol']}")
-            # Check if quantity is less than 0 and the product is 'MIS', indicating a buy order for MIS positions
-            if position['quantity'] < 0 and position['product'] == 'MIS':
-                order_id = broker.order_place(
-                    tradingsymbol=position['tradingsymbol'],
-                    exchange=position['exchange'],
-                    transaction_type='BUY',  # Change to BUY for buying positions
-                    quantity=int(-1 * position['quantity']),  # Buying the existing position
-                    order_type='MARKET',
-                    product='MIS',
-                    variety='regular'
-                )
-                if order_id:
-                    logging.info(f"Order {order_id} placed successfully for {position['tradingsymbol']}")
-                else:
-                    logging.error("Order placement failed")
+            order_id = broker.order_place(
+                tradingsymbol=position['tradingsymbol'],
+                exchange=position['exchange'],
+                transaction_type='BUY',  # Change to BUY for buying positions
+                quantity=int(-1 * position['quantity']),  # Buying the existing position
+                order_type='MARKET',
+                product='MIS',
+                variety='regular'
+            )
+            if order_id:
+                logging.info(f"Order {order_id} placed successfully for {position['tradingsymbol']}")
+            else:
+                logging.error("Order placement failed")
     except Exception as e:
         print(traceback.format_exc())
         logging.error(f"{str(e)} while placing orders")
@@ -43,16 +41,18 @@ except Exception as e:
     print(traceback.format_exc())
     logging.error(f"{str(e)} Unable to get KiteConnect instance")
 
-# Get open MIS positions
-mis_positions = pd.DataFrame([pos for pos in broker.positions()['day'] if pos['product'] == 'MIS'])
+# Get open MIS positions with quantity < 0
+positions_response = broker.kite.positions()['net']
+mis_positions = pd.DataFrame([pos for pos in positions_response if pos['product'] == 'MIS' and pos['quantity'] < 0])
 
 # Call the function with the appropriate arguments
 result = place_mis_orders(mis_positions, broker)
 
 # Do something with the result if needed
 if result:
-    print("Buy orders placed successfully for MIS positions.")
+    print("Buy orders placed successfully for MIS positions with quantity < 0.")
 else:
-    print("Failed to place buy orders for MIS positions.")
+    print("Failed to place buy orders for MIS positions with quantity < 0.")
+
 
 
