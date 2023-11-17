b453b8ce8941b36c03115eb86df879dce01fcaf3
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 15:07:05 +0530

    Update adptpxy.py

--- sys/exe/run/adptpxy.py
+++ sys/exe/run/adptpxy.py
@@ -380,9 +380,8 @@ try:
                         row['qty'] > 0 and
                         row['source'] == 'positions' and
                         row['product'] == 'MIS' and
-                        row['PnL%'] < 0 and 
                         #(row['PnL%'] > row['Yi']) or ((row['PnL%'] > row['Xl']) and (mktpxy == 'Sell' or mktpxy == 'Bear'))
-                        row['PnL%'] < 0
+                        row['PnL%'] > 1
                     ):
                         # Print the row before placing the order
                         print(row)
@@ -402,8 +401,29 @@ try:
                             print(f"An unexpected error occurred while placing an order for key {key}: {e}")
 
 
-
-
+                    elif (
+                        row['qty'] < 0 and
+                        row['source'] == 'positions' and
+                        row['product'] == 'MIS' and
+                        #(row['PnL%'] > row['Yi']) or ((row['PnL%'] > row['Xl']) and (mktpxy == 'Sell' or mktpxy == 'Bear'))
+                        row['PnL%'] < 0
+                    ):
+                        # Print the row before placing the order
+                        print(row)
+    
+                        try:
+                            is_placed = mis_order_buy(key, row)
+                            if is_placed:
+                                # Write the row to the CSV file here
+                                with open(csv_file_path, 'a', newline='') as csvfile:
+                                    csvwriter = csv.writer(csvfile)
+                                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                        except InputException as e:
+                            # Handle the specific exception and print only the error message
+                            print(f"An error occurred while placing an order for key {key}: {e}")
+                        except Exception as e:
+                            # Handle any other exceptions that may occur during order placement
+                            print(f"An unexpected error occurred while placing an order for key {key}: {e}")
 
 
 
