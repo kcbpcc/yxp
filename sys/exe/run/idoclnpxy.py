b99d3caf7b86f8d4d4c86921d98a2d7464357a86
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 10:22:22 +0530

    Create idoclnpxy.py

---
+++ sys/exe/run/idoclnpxy.py
@@ -0,0 +1,24 @@
+import csv
+from datetime import datetime, timedelta
+
+# Specify the path to your CSV file
+file_path = 'filePnL.csv'
+
+# Function to check if the current time is between 0:00 AM and 11:55 PM UTC
+def is_time_between(start_time, end_time):
+    current_time = datetime.utcnow()
+    return start_time <= current_time <= end_time
+
+# Specify the time range during which you want to delete contents (2:00 AM to 3:00 AM UTC)
+start_time = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+end_time = datetime.utcnow().replace(hour=23, minute=59, second=0, microsecond=0)
+
+# Check if the current time is within the specified range
+if is_time_between(start_time, end_time):
+    # Open the file in write mode to truncate its contents
+    with open(file_path, 'w', newline='') as csvfile:
+        # Create a CSV writer object
+        csv_writer = csv.writer(csvfile)
+
+        # Write an empty row to clear the contents
+        csv_writer.writerow([])
