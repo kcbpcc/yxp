464e09467575dc6881e255d312e4123bfda852a5
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 20:10:40 +0530

    Update adptpxy.py

--- sys/exe/run/adptpxy.py
+++ sys/exe/run/adptpxy.py
@@ -275,22 +275,59 @@ try:
     # Assuming NIFTY is a dictionary-like object with pandas Series
     import pandas as pd
     
+    # Calculate Precise
     Precise = min(1.3, (1 + NIFTY['strength']).round(1).max())
-    Xlratd = NIFTY['strength'] * timpxy
-    Yield = timpxy
     
-    conditions_pxy = [(mktpxy == 'Bull') | (mktpxy == 'Buy'), (mktpxy == 'Sell'), (mktpxy == 'Bear'), (mktpxy == 'Bull')]
+    # Calculate Xlratd
+    Xlratd = NIFTY['strength'] * timpxy
+    
+    # Calculate Yield
+    Yield = timpxy * (-1)
+    
+    # Conditions for PXY
+    conditions_pxy = [
+        (mktpxy.isin(['Bull', 'Buy'])),
+        (mktpxy == 'Sell'),
+        (mktpxy == 'Bear'),
+        (mktpxy == 'Bull')
+    ]
+    
+    # Choices for PXY
     choices_pxy = ['Yield', 'Xlratd', 'Precise', 'Yield']
+    
+    # Calculate PXY
     PXY = np.select(conditions_pxy, choices_pxy)
-
     
     # Assuming NIFTY['Day_Change_%'] is a Pandas Series
+    # Calculate _Precise, _Xlratd, _Yield, and _PXY
     _Precise = min(1.3, (NIFTY['weakness']).round(1).max(), -1)
     _Xlratd = (NIFTY['weakness'] * timpxy).clip(None, -1)
     _Yield = timpxy * (-1)
-    _conditions_pxy = [(mktpxy == 'Bear') | (mktpxy == 'Buy'), (mktpxy == 'Sell'), (mktpxy == 'Bear'), (mktpxy == 'Bull')]
+    
+    # Conditions for _PXY
+    _conditions_pxy = [
+        (mktpxy == 'Bear') | (mktpxy == 'Buy'),
+        (mktpxy == 'Sell'),
+        (mktpxy == 'Bear'),
+        (mktpxy == 'Bull')
+    ]
+    
+    # Choices for _PXY
     _choices_pxy = ['_Yield', '_Xlratd', '_Precise', '_Yield']
+    
+    # Calculate _PXY
     _PXY = np.select(_conditions_pxy, _choices_pxy)
+    
+    # Print values
+    print("Precise:", Precise)
+    print("Xlratd:", Xlratd)
+    print("Yield:", Yield)
+    print("PXY:", PXY)
+    
+    print("_Precise:", _Precise)
+    print("_Xlratd:", _Xlratd)
+    print("_Yield:", _Yield)
+    print("_PXY:", _PXY)
     
 
 
