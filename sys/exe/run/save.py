b922e19a693e556489ea71f3371a0bf7f772d60e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-17 12:44:36 +0530

    Update save.py

--- sys/exe/run/save.py
+++ sys/exe/run/save.py
@@ -24,3 +24,27 @@
         except Exception as e:
             # Handle any other exceptions that may occur during the loop
             print(f"An unexpected error occurred: {e}")
+                    elif (
+                        row['source'] == 'positions' and
+                        row['product'] == 'MIS' and
+                        row['qty'] > 0 and
+                        row['PnL%'] > 1.4 and 
+                        (row['PnL%'] > row['Yi']) or ((row['PnL%'] > row['Xl']) and (mktpxy == 'Sell' or mktpxy == 'Bear'))
+
+                    ):
+                        # Print the row before placing the order
+                        print(row)
+    
+                        try:
+                            is_placed = mis_order_sell(key, row)
+                            if is_placed:
+                                # Write the row to the CSV file here
+                                with open(csv_file_path, 'a', newline='') as csvfile:
+                                    csvwriter = csv.writer(csvfile)
+                                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                        except InputException as e:
+                            # Handle the specific exception and print only the error message
+                            print(f"An error occurred while placing an order for key {key}: {e}")
+                        except Exception as e:
+                            # Handle any other exceptions that may occur during order placement
+                            print(f"An unexpected error occurred while placing an order for key {key}: {e}")
