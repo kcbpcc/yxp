817a273d226633d757fb6a612f12763b6e713f65
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-19 06:52:08 +0530

    Create lupyxy.py

---
+++ sys/exe/run/lupyxy.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+def calculate_loop_duration(current_time):
+    # Define time intervals
+    interval_1_start = datetime.strptime("09:13", "%H:%M").time()
+    interval_1_end = datetime.strptime("01:00", "%H:%M").time()
+    
+    interval_2_start = datetime.strptime("02:44", "%H:%M").time()
+    interval_2_end = datetime.strptime("03:12", "%H:%M").time()
+
+    # Convert current time to time object
+    current_time = current_time.time()
+
+    # Check if current time is within the defined intervals
+    if interval_1_start <= current_time <= interval_1_end or interval_2_start <= current_time <= interval_2_end:
+        return 1
+    else:
+        return 15
+
+# Get the current UTC time
+current_utc_time = datetime.utcnow()
+
+# Calculate loop duration based on current time
+loop_duration = calculate_loop_duration(current_utc_time)
+
+print(f"Current UTC time: {current_utc_time.strftime('%H:%M')}")
+print(f"Loop duration: {loop_duration} seconds")
