a3e7b72f0ca5dd6e4f2bba42fb79cc62fc47cfea
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-21 15:27:05 +0530

    Update ynfndpxy.py

--- sys/exe/run/ynfndpxy.py
+++ sys/exe/run/ynfndpxy.py
@@ -26,7 +26,7 @@ def calculate_decision():
         available_cash = response["equity"]["available"]["live_balance"]
         print(f"Available Cash: {available_cash}")
         # Define 'YES' or 'NO' based on the available cash
-        decision = "YES" if available_cash > 500000 else "NO"
+        decision = "YES" if available_cash > 10000 else "NO"
 
         return decision
         return available_cash
