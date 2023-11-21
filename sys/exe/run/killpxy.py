5424ecf7c350c710dc8a8f5d6a000c1bb024e4bc
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-21 14:56:12 +0530

    Update killpxy.py

--- sys/exe/run/killpxy.py
+++ sys/exe/run/killpxy.py
@@ -368,11 +368,7 @@ try:
                         print(row)
                         try:
                             is_placed = mis_order_sell(key, row)
-                            if is_placed:
-                                # Write the row to the CSV file here
-                                with open(csv_file_path, 'a', newline='') as csvfile:
-                                    csvwriter = csv.writer(csvfile)
-                                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+
                         except InputException as e:
                             # Handle the specific exception and print only the error message
                             print(f"An error occurred while placing an order for key {key}: {e}")
@@ -392,11 +388,7 @@ try:
     
                         try:
                             is_placed = mis_order_buy(key, row)
-                            if is_placed:
-                                # Write the row to the CSV file here
-                                with open(csv_file_path, 'a', newline='') as csvfile:
-                                    csvwriter = csv.writer(csvfile)
-                                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+
                         except InputException as e:
                             # Handle the specific exception and print only the error message
                             print(f"An error occurred while placing an order for key {key}: {e}")
