77dd20e6b165650529a1a86c4e5a0ed0bf516164
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-21 07:34:51 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -216,16 +216,17 @@ try:
     combined_df['xl'] = (1.4 + (combined_df['pr'] * 2)).clip(lower=1.4).round(2)
     combined_df['yi'] = (1.4 + (combined_df['pr'] * 3)).clip(lower=1.4).round(2)
     
-    combined_df['pxy'] = np.where(mktcombined_df['pxy'].isin(["Buy", "Bull"]), combined_df['yi'],
-                                  np.where(mktcombined_df['pxy'] == "Sell", combined_df['xl'], 1))
+    combined_df['pxy'] = np.where(combined_df['pxy'].isin(["Buy", "Bull"]), combined_df['yi'],
+                                  np.where(combined_df['pxy'] == "Sell", combined_df['xl'], 1))
     
     epsilon = 0.01  # Define epsilon if not already defined
     combined_df['_pr'] = min(-0.1, round((0.0 + (combined_df['weakness'] * 1.0)).min(), 2) - epsilon)
     combined_df['_xl'] = (combined_df['_pr'] * 2 - 1).clip(upper=-1.4).round(2)
     combined_df['_yi'] = (combined_df['_pr'] * 3 - 1).clip(upper=-1.4).round(2)
     
-    combined_df['yep'] = np.where(mktcombined_df['pxy'].isin(["Sell", "Bear"]), combined_df['_yi'],
-                                   np.where(mktcombined_df['pxy'] == "Buy", combined_df['_xl'], -1))
+    combined_df['yep'] = np.where(combined_df['pxy'].isin(["Sell", "Bear"]), combined_df['_yi'],
+                                   np.where(combined_df['pxy'] == "Buy", combined_df['_xl'], -1))
+
     
 
 
