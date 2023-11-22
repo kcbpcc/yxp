03efbc3ba4775a26a9ef1f4f0cb9b436df16a233
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-22 15:03:57 +0530

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -22,6 +22,7 @@ while True:
     subprocess.run(['python3', 'clnpxy.py'])
     subprocess.run(['python3', 'dshpxy.py'])
     subprocess.run(['python3', 'tblprftpxy.py'])
+    subprocess.run(['python3', 'nitebuypxy.py'])
 
     # Set the python3IOENCODING environment variable to 'utf-8'
     sys.stdout.reconfigure(encoding='utf-8')
