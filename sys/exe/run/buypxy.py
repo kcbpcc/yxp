acf78c8362d6d11c3e2fa4c79a69ab1ffc979c8f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-23 18:35:06 +0530

    Add files via upload

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -9,10 +9,7 @@ import pandas as pd
 import traceback
 import sys
 import os
-from mktchksmbl import smktchk
-
-# Set the intervals
-intervals = [5]
+from mktchksmbl import get_market_sentiment
 
 logging = Logger(10)
 holdings = dir_path + "holdings.csv"
@@ -21,7 +18,6 @@ black_file = dir_path + "blacklist.txt"
 try:
     sys.stdout = open('output.txt', 'w')
     broker = get_kite(api="bypass", sec_dir=dir_path)
-
     if fileutils.is_file_not_2day(holdings):
         logging.debug("getting holdings for the day ...")
         resp = broker.kite.holdings()
@@ -29,131 +25,83 @@ try:
             df = get(resp)
             logging.debug(f"writing to csv ... {holdings}")
             df.to_csv(holdings, index=False)
-        with open(black_file, 'w+'):
+        with open(black_file, 'w+') as bf:
             pass
 except Exception as e:
+    print(traceback.format_exc())
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
 
 # Call the calculate_decision function to get the decision
 decision = calculate_decision()
 
-if decision == "YES" and mktpxy in ['Buy', 'Bull', 'Bear', 'Sell']:
-    try:
-        lst = []
-        file_size_in_bytes = os.path.getsize(holdings)
-        logging.debug(f"holdings file size: {file_size_in_bytes} bytes")
-        
-        if file_size_in_bytes > 50:
-            logging.debug(f"reading from csv ...{holdings}")
-            df_holdings = pd.read_csv(holdings)
-            
-            if not df_holdings.empty:
-                lst = df_holdings['tradingsymbol'].to_list()
+if decision == "YES":
+    for symbol_to_check in lst_tlyne:
+        # Append '.NS' to the symbol if it doesn't have it already
+        symbol_to_check_ns = symbol_to_check if '.NS' in symbol_to_check else f"{symbol_to_check}.NS"
 
-        # get list from Trendlyne
-        lst_tlyne = []
-        lst_dct_tlyne = Trendlyne().entry()
-        
-        if lst_dct_tlyne and any(lst_dct_tlyne):
-            print(pd.DataFrame(lst_dct_tlyne).set_index('tradingsymbol').rename_axis('Trendlyne'), "\n")
-            lst_tlyne = [dct['tradingsymbol'] for dct in lst_dct_tlyne]
-    except Exception as e:
-        logging.error(f"{str(e)} unable to read holdings or Trendlyne calls")
-        sys.exit(1)
+        # Get the market sentiment for each symbol
+        symbol_sentiment = get_market_sentiment(symbol_to_check_ns)
 
-    try:
-        if any(lst_tlyne):
-            logging.info(f"reading trendlyne ...{lst_tlyne}")
-            lst_tlyne = [x for x in lst_tlyne if x not in lst]
-            logging.info(f"filtered from holdings: {lst}")
+        # Check if the market sentiment is 'Bull'
+        if symbol_sentiment == 'Bull':
+            try:
+                lst = []
+                file_size_in_bytes = os.path.getsize(holdings)
+                logging.debug(f"holdings file size: {file_size_in_bytes} bytes")
+                if file_size_in_bytes > 50:
+                    logging.debug(f"reading from csv ...{holdings}")
+                    df_holdings = pd.read_csv(holdings)
+                    if not df_holdings.empty:
+                        lst = df_holdings['tradingsymbol'].to_list()
 
-            # get list from positions
-            lst_dct = broker.positions
-            
-            if lst_dct and any(lst_dct):
-                lst = [dct['symbol'] for dct in lst_dct]
-                lst_tlyne = [x for x in lst_tlyne if x not in lst]
-                logging.info(f"filtered from positions ...{lst}")
-    except Exception as e:
-        logging.error(f"{str(e)} unable to read positions")
-        sys.exit(1)
+                # get list from positions
+                lst_dct = broker.positions
+                if lst_dct and any(lst_dct):
+                    lst = [dct['symbol'] for dct in lst_dct]
+                    logging.info(f"filtered from positions ...{lst}")
+            except Exception as e:
+                print(traceback.format_exc())
+                logging.error(f"{str(e)} unable to read positions")
+                sys.exit(1)
 
-    def transact(dct):
-        try:
-            def get_ltp():
-                ltp = -1
-                key = "NSE:" + dct['tradingsymbol']
+            # Place a buy order for the symbol
+            try:
+                # Get the last traded price (LTP) for the symbol
+                key = "NSE:" + symbol_to_check_ns
                 resp = broker.kite.ltp(key)
-                
-                if resp and isinstance(resp, dict) and key in resp:
+                if resp and isinstance(resp, dict):
                     ltp = resp[key]['last_price']
-                
-                return ltp
 
-            ltp = get_ltp()
-            logging.info(f"LTP for {dct['tradingsymbol']} is {ltp}")
-            
-            if ltp <= 0:
-                return dct['tradingsymbol']
+                    # Place a buy order
+                    order_id = broker.order_place(
+                        tradingsymbol=symbol_to_check_ns,
+                        exchange='NSE',
+                        transaction_type='BUY',
+                        quantity=your_quantity,  # Replace with the desired quantity
+                        order_type='MARKET',
+                        product='MIS',
+                        variety='regular',
+                        price=round_to_paise(ltp, +0.1)
+                    )
 
-            # Check market sentiment
-            yahoo_symbol = dct['tradingsymbol'] + ".NS"
-            market_sentiment = smktchk(yahoo_symbol, intervals[0])
+                    if order_id:
+                        logging.info(
+                            f"BUY {order_id} placed for {symbol_to_check_ns} successfully")
+                    else:
+                        print(traceback.format_exc())
+                        logging.error(
+                            f"Unable to place order for {symbol_to_check_ns}")
 
-            if market_sentiment not in ['Buy', 'Bull']:
-                logging.info(f"Skipping order for {yahoo_symbol}. Market sentiment is {market_sentiment}")
-                return dct['tradingsymbol']
-
-            order_id = broker.order_place(
-                tradingsymbol=dct['tradingsymbol'],
-                exchange='NSE',
-                transaction_type='BUY',
-                quantity=int(float(dct['QTY'].replace(',', ''))),
-                order_type='MARKET',
-                product='MIS',
-                variety='regular',
-                price=round_to_paise(ltp, +0.1)
-            )
-
-            if order_id:
-                logging.info(f"BUY {order_id} placed for {yahoo_symbol} successfully")
-            else:
-                logging.error(f"Unable to place order for {yahoo_symbol}")
-
-        except Exception as e:
-            logging.error(f"{str(e)} while placing order for {yahoo_symbol}")
-
-        return dct['tradingsymbol']
-
-    if any(lst_tlyne):
-        new_list = []
-        
-        # Filter the original list based on the subset of 'tradingsymbol' values
-        lst_all_orders = [d for d in lst_dct_tlyne if d['tradingsymbol'] in lst_tlyne]
-        
-        # Read the list of previously failed symbols from the file
-        with open(black_file, 'r') as file:
-            lst_failed_symbols = [line.strip() for line in file.readlines()]
-        
-        logging.info(f"ignored symbols: {lst_failed_symbols}")
-        lst_orders = [d for d in lst_all_orders if d['tradingsymbol'] not in lst_failed_symbols]
-        
-        for d in lst_orders:
-            failed_symbol = transact(d)
-            
-            if failed_symbol:
-                new_list.append(failed_symbol)
-            
-            Utilities().slp_til_nxt_sec()
-
-        if any(new_list):
-            with open(black_file, 'w') as file:
-                for symbol in new_list:
-                    file.write(symbol + '\n')
+            except Exception as e:
+                print(traceback.format_exc())
+                logging.error(
+                    f"{str(e)} while processing symbol {symbol_to_check_ns}")
+        else:
+            # Perform actions when the market sentiment is not Bull
+            print(
+                f"\033[91mThe market sentiment for {symbol_to_check_ns} is not Bull\033[0m")
 
 elif decision == "NO":
     # Perform actions for "NO"
     print("\033[91mNo Funds Available \033[0m")
-
-
