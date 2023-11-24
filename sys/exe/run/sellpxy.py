353825238cf0fc44d294fc41f423a68fe123d64f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-24 17:03:11 +0530

    Update sellpxy.py

--- sys/exe/run/sellpxy.py
+++ sys/exe/run/sellpxy.py
@@ -16,7 +16,7 @@ from mktchksmbl import getsmktchk
 
 
 logging = Logger(10)
-holdings = dir_path + "fileHPdf.csv"
+holdings = dir_path + "holdings.csv"
 black_file = dir_path + "blacklist.txt"
 try:
     sys.stdout = open('output.txt', 'w')
