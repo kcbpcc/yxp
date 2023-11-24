0aad6954bd354f6e943994f8c7096c579ebb205a
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-24 21:54:08 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -472,7 +472,7 @@ try:
         
         #print(left_aligned_format.format(f"Bear Power:{BRIGHT_GREEN if (_Pr > 0.5) and isinstance(_Pr, (int, float)) else BRIGHT_RED}{'---' if not isinstance(_Pr, (int, float)) else round(_Pr, 2)}{RESET}"), end="")
         #print(right_aligned_format.format(f"Bull Power:{BRIGHT_GREEN if (Pr > 0.5) and isinstance(Pr, (int, float)) else BRIGHT_RED}{'---' if not isinstance(Pr, (int, float)) else round(Pr, 2)}{RESET}"))
-        print(left_aligned_format.format(f"Day Change%:{BRIGHT_GREEN if NIFTY['Day_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Day_Change_%'][0], 2)}{RESET}"), end="")
+        #print(left_aligned_format.format(f"Day Change%:{BRIGHT_GREEN if NIFTY['Day_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Day_Change_%'][0], 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL:{BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{round(total_dPnL, 2)}{RESET}"))
         print(left_aligned_format.format(f"Day Status:{BRIGHT_GREEN if NIFTY['Day Status'][0] in ('Bull', 'sBull') else BRIGHT_RED}{NIFTY['Day Status'][0]}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL%:{BRIGHT_GREEN if total_dPnL_percentage > 0 else BRIGHT_RED}{round(total_dPnL_percentage, 2)}{RESET}"))
