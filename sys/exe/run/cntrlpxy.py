b0e402dfcf32378881db414bc160bbf94908a4a0
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-25 17:29:43 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -493,7 +493,8 @@ try:
         left_aligned_format = "{:<" + str(column_width) + "}"
         right_aligned_format = "{:>" + str(column_width) + "}"
         
-        print(left_aligned_format.format(f"switch:{switch}{RESET}"), end="")
+        # Print statements
+        print(left_aligned_format.format(f"Switch:{switch}{RESET}"), end="")
         print(right_aligned_format.format(f"Power:{BRIGHT_GREEN if power > 0.5 else BRIGHT_RED}{power}{RESET}"))
         print(left_aligned_format.format(f"Day Change%:{BRIGHT_GREEN if NIFTY['Day_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Day_Change_%'][0], 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL:{BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{round(total_dPnL, 2)}{RESET}"))
