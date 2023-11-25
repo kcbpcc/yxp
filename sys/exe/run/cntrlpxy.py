3b3378cfc3309bc88ef9d43587684056f43f95e6
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-25 12:30:28 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -330,7 +330,7 @@ try:
     # Calculate the metrics
     
     epsilon = 1e-10
-    NIFTY['strength'] = ((NIFTY['ltp'] - (NIFTY['low'] - 0.01)) / (abs(NIFTY['high'] + 0.01) - abs(NIFTY['low'] - 0.01)))    
+    Power = ((NIFTY['ltp'] - (NIFTY['low'] - 0.01)) / (abs(NIFTY['high'] + 0.01) - abs(NIFTY['low'] - 0.01)))    
     NIFTY['weakness'] = ((NIFTY['ltp'] - (NIFTY['high'] - 0.01)) / (abs(NIFTY['high'] + 0.01) - abs(NIFTY['low'] - 0.01)))
     
     # Define the file path for the CSV file
@@ -486,11 +486,9 @@ try:
         left_aligned_format = "{:<" + str(column_width) + "}"
         right_aligned_format = "{:>" + str(column_width) + "}"
         
-        print(left_aligned_format.format(
-            f"Bear Power: {BRIGHT_GREEN if (NIFTY['strength'] > 0.5).any() and isinstance(NIFTY['strength'], (int, float)) else BRIGHT_RED}"
-            f"{'---' if not isinstance(NIFTY['strength'], (int, float)) else round(NIFTY['strength'], 2)}{RESET}"
-        ), end="")
-        print(right_aligned_format.format(f"Power:{BRIGHT_GREEN if (NIFTY['strength'] > 0.5) and isinstance(NIFTY['strength'], (int, float)) else BRIGHT_RED}{'---' if not isinstance(NIFTY['strength'], (int, float)) else round(NIFTY['strength'], 2)}{RESET}"))
+        print(left_aligned_format.format(f"Power:{BRIGHT_GREEN if Power >= 0 else BRIGHT_RED}{Power}{RESET}"), end="")
+        print(right_aligned_format.format(f"Power:{BRIGHT_GREEN if Power > .50 else BRIGHT_RED}{Power}{RESET}"))      
+        
         print(left_aligned_format.format(f"Day Change%:{BRIGHT_GREEN if NIFTY['Day_Change_%'][0] >= 0 else BRIGHT_RED}{round(NIFTY['Day_Change_%'][0], 2)}{RESET}"), end="")
         print(right_aligned_format.format(f"dPnL:{BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{round(total_dPnL, 2)}{RESET}"))
         print(left_aligned_format.format(f"Day Status:{BRIGHT_GREEN if NIFTY['Day Status'][0] in ('Bull', 'SuperBull') else BRIGHT_RED}{NIFTY['Day Status'][0]}{RESET}"), end="")
