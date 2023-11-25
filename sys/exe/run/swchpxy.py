21a415eb89d29e411317482dc54f07dc3e889c5f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-25 17:17:25 +0530

    Create swchpxy.py

---
+++ sys/exe/run/swchpxy.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+from datetime import datetime
+
+def analyze_stock(symbol):
+    # Get today's date
+    today_date = datetime.now().strftime('%Y-%m-%d')
+
+    # Fetch historical stock data for today
+    stock_data = yf.download(symbol, start=today_date, end=today_date)
+
+    # Check if the conditions are met
+    below_open_high_above_open = (stock_data['Low'] < stock_data['Open']) & (stock_data['High'] > stock_data['Open'])
+    above_open_low_below_open = (stock_data['High'] > stock_data['Open']) & (stock_data['Low'] < stock_data['Open'])
+
+    if below_open_high_above_open.any() or above_open_low_below_open.any():
+        return 'yes'
+    else:
+        return 'no'
+
+if __name__ == "__main__":
+    # Replace 'AAPL' with the stock symbol you're interested in
+    stock_symbol = 'AAPL'
+
+    # Analyze stock data for today
+    result = analyze_stock(stock_symbol)
+
+    # Display the result
+    print(result)
