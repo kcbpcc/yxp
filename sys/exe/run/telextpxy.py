3dd26ee83c4b7f220e2734aab0d25377ed3ef1c6
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 16:49:39 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -1,29 +1,43 @@
 import telegram
 import asyncio
+import pandas as pd
 
-def send_telegram_notification(row, key):
+def send_telegram_message(message_text, bot_token, user_usernames):
     try:
-        message_text = f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
-        
-        # Define the bot token and your Telegram username or ID
-        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
-        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
-        
         # Function to send a message to Telegram
-        async def send_telegram_notification(message_text):
+        async def send_message():
             bot = telegram.Bot(token=bot_token)
             await bot.send_message(chat_id=user_usernames, text=message_text)
-        
+
+        # Send the message to Telegram
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(send_message())
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
 
-    # Send the 'row' content as a message to Telegram immediately after printing the row
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(send_telegram_notification(message_text))
+def process_new_rows(file_path, telblock_path, bot_token, user_usernames):
+    # Read telblock file to get the list of already processed rows
+    try:
+        with open(telblock_path, 'r') as telblock_file:
+            telblock = telblock_file.read().splitlines()
+    except FileNotFoundError:
+        telblock = []
 
-# Example of calling the function
-if __name__ == "__main__":
-    row_data = "Your Row Data"  # Replace with your actual row data
-    key_data = "Your Key Data"  # Replace with your actual key data
-    send_telegram_notification(row_data, key_data)
+    # Read the CSV file
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return
+
+    # Iterate over new rows
+    for index, row in df.iterrows():
+        row_str = str(row)
+        if row_str not in telblock:
+            message_text = f"{row_str} Symbol: {row['symbol']} https://www.tradingview.com/chart/?symbol={row['symbol']}"
+            send_telegram_message(message_text, bot_token, user_usernames)
+            # Append the processed row to telblock
+            with open(telblock_path, 'a') as telblock_file:
+                telblock_file.write(row_str + '\n')
+
