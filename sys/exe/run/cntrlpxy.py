ca6b2c243f238783cd75fb0b32c92fcb9063483f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 16:37:29 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -26,7 +26,7 @@ except Exception as e:
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
 
-file_path = 'filePnL.csv'
+file_path = 'telegramext.csv'
 ###########################################################################################################################################################################################################
 def order_place(index, row):
     try:
