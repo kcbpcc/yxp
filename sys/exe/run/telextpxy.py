d142286a0305769068d0c93643f44967e4217b68
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 16:25:57 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -1,7 +1,7 @@
 import telegram
 import asyncio
 
-def send_message_to_telegram(row, key):
+def send_telegram_notification(row, key):
     try:
         message_text = f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
         
@@ -10,7 +10,7 @@ def send_message_to_telegram(row, key):
         user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
         
         # Function to send a message to Telegram
-        async def send_telegram_message(message_text):
+        async def send_telegram_notification(message_text):
             bot = telegram.Bot(token=bot_token)
             await bot.send_message(chat_id=user_usernames, text=message_text)
         
@@ -20,10 +20,10 @@ def send_message_to_telegram(row, key):
 
     # Send the 'row' content as a message to Telegram immediately after printing the row
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(send_telegram_message(message_text))
+    loop.run_until_complete(send_telegram_notification(message_text))
 
 # Example of calling the function
 if __name__ == "__main__":
     row_data = "Your Row Data"  # Replace with your actual row data
     key_data = "Your Key Data"  # Replace with your actual key data
-    send_message_to_telegram(row_data, key_data)
+    send_telegram_notification(row_data, key_data)
