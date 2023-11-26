3fa68d32e06c4f8ecbbb043f0cbfa9bd056d24c8
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 16:22:28 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -1,20 +1,29 @@
-# telegram_notification.py
-
+import telegram
 import asyncio
-from telegram import Bot
 
-# Move the token, usernames, and message_text definition here
-bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
-user_usernames = '-4022487175'
-
-async def send_telegram_notification(row):
-    async def create_message_text(row):
-        return f"{str(row)} Symbol: {key} [TradingView Chart](https://www.tradingview.com/chart/?symbol={key})"
-
+def send_message_to_telegram(row, key):
     try:
-        message_text = await create_message_text(row, key)
-        bot = Bot(token=bot_token)
-        bot.send_message(chat_id=user_usernames, text=message_text)
+        message_text = f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
+        
+        # Define the bot token and your Telegram username or ID
+        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+        
+        # Function to send a message to Telegram
+        async def send_telegram_message(message_text):
+            bot = telegram.Bot(token=bot_token)
+            await bot.send_message(chat_id=user_usernames, text=message_text)
+        
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
+
+    # Send the 'row' content as a message to Telegram immediately after printing the row
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_telegram_message(message_text))
+
+# Example of calling the function
+if __name__ == "__main__":
+    row_data = "Your Row Data"  # Replace with your actual row data
+    key_data = "Your Key Data"  # Replace with your actual key data
+    send_message_to_telegram(row_data, key_data)
