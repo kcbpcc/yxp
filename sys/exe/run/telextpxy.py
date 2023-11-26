05bc6afd937c7f0f355c45634d7db94bbcc08371
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 11:03:03 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -2,7 +2,6 @@
 
 import asyncio
 from telegram import Bot
-from telegram import ParseMode
 
 # Move the token, usernames, and message_text definition here
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
@@ -15,7 +14,7 @@ async def send_telegram_notification(row):
     try:
         message_text = await create_message_text(row, key)
         bot = Bot(token=bot_token)
-        bot.send_message(chat_id=user_usernames, text=message_text, parse_mode=ParseMode.MARKDOWN)
+        bot.send_message(chat_id=user_usernames, text=message_text)
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
