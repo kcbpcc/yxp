c67cd1425c8bc1fc9e54355cf50f0df1425835c3
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 08:59:13 +0530

    Update tstpxy.py

--- sys/exe/run/tstpxy.py
+++ sys/exe/run/tstpxy.py
@@ -5,7 +5,7 @@ import subprocess
 directory = os.path.dirname(os.path.realpath(__file__))
 
 # Get a list of all Python files in the directory (excluding pxy.py)
-python_files = [f for f in os.listdir(directory) if f.endswith(".py") and f != "pxy.py"]
+python_files = [f for f in os.listdir(directory) if f.endswith(".py") and f != "pxy.py" and f != "tstpxy.py"]
 
 # Variables to track success and failure
 success_files = []
