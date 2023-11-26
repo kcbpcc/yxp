b83666cc170099e7a2dccee409401478ec3b2aa9
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-26 23:16:16 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -7,6 +7,11 @@ import pandas as pd
 BOT_TOKEN = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
 USER_USERNAMES = '-4022487175'  # Replace with your Telegram username or ID
 
+
+HEADER_LIST = ['qty', 'avg', 'close', 'ltp', 'open', 'high', 'low', 'PnL%_H', 'dPnL%', 'product', 'source', 'key', 'pxy', 'yxp', 'PnL%', 'PnL%']
+FILE_PATH = 'filePnL.csv'
+BLOCK_FILE_PATH = 'telblock.txt'
+
 def send_telegram_message(message_text, bot_token, user_usernames):
     try:
         # Function to send a message to Telegram
@@ -14,56 +19,47 @@ def send_telegram_message(message_text, bot_token, user_usernames):
             bot = telegram.Bot(token=bot_token)
             await bot.send_message(chat_id=user_usernames, text=message_text)
 
-        # Send the message to Telegram
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(send_message())
+        # Run the asyncio event loop directly with asyncio.run
+        asyncio.run(send_message())
     except Exception as e:
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
 
-def process_new_rows(file_path, telblock_path, bot_token, user_usernames):
-    # Read telblock file to get the list of already processed keys
+def process_new_rows(file_path, block_file_path, bot_token, user_usernames, header_list):
+    # Read block file to get the list of already processed keys
     try:
-        with open(telblock_path, 'r') as telblock_file:
-            telblock = telblock_file.read().splitlines()
+        with open(block_file_path, 'r') as block_file:
+            block_list = block_file.read().splitlines()
     except FileNotFoundError:
-        telblock = []
+        block_list = []
 
     # Read the CSV file
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path, header=None, names=header_list)
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return
 
-    # Print column names
-    print("Column Names:", df.columns)
-
-    # Check if 'KeyColumn' is in the DataFrame
-    key_column = 'KeyColumn'  # Replace with the actual column name
-    if key_column not in df.columns:
-        print(f"Error: '{key_column}' not found in DataFrame columns.")
-        return
-
     # Iterate over rows
     for index, row in df.iterrows():
-        # Assuming 'KeyColumn' is the column containing key information
-        key_parts = row[key_column].split(':')
-        
-        if key_parts[0] not in telblock:
-            message_text = f"{', '.join([f'{key.strip()}: {value.strip()}' for key, value in zip(key_parts[::2], key_parts[1::2])])} https://www.tradingview.com/chart/?symbol={key_parts[0]}"
+        key = row['key']
+        # Extract stock code from the 'key' column (removing 'NSE:' or 'BSE:')
+        stock_code = key.split(':')[-1]
+
+        if stock_code not in block_list:
+            # Construct message text with row data and headers
+            message_text = f"{', '.join([f'{header}: {value}' for header, value in zip(header_list, row.values)])} https://www.tradingview.com/chart/?symbol={stock_code}"
+            
+            # Send message to Telegram
             send_telegram_message(message_text, bot_token, user_usernames)
-            # Append the processed key to telblock
-            with open(telblock_path, 'a') as telblock_file:
-                telblock_file.write(key_parts[0] + '\n')
 
-# File paths
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-FILE_PATH = os.path.join(CURRENT_DIR, 'filePnL.csv')
-TELBLOCK_PATH = os.path.join(CURRENT_DIR, 'telblock.txt')
+            # Append the processed stock code to block list
+            with open(block_file_path, 'a') as block_file:
+                block_file.write(stock_code + '\n')
 
-# Execute the functionality automatically
-process_new_rows(FILE_PATH, TELBLOCK_PATH, BOT_TOKEN, USER_USERNAMES)
+# Execute the functionality
+if __name__ == "__main__":
+    process_new_rows(FILE_PATH, BLOCK_FILE_PATH, BOT_TOKEN, USER_USERNAMES, HEADER_LIST)
 
 
 
