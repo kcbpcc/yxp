fba76d29a7e8b5e22a9ffccbf5f57ea3df54bee1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 11:45:34 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -8,11 +8,12 @@ bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
 user_usernames = ('-4022487175')
 
 def send_message(bot, chat_id, text):
-    bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
+    # Specify 'Markdown' as the parse mode
+    bot.send_message(chat_id=chat_id, text=text, parse_mode='Markdown')
 
 async def send_messages():
     # Read CSV file
-    csv_file_path = 'filePnL.csv'
+    csv_file_path = 'PnL.csv'
     df = pd.read_csv(csv_file_path)
 
     # Check for headers in the CSV file
@@ -57,5 +58,3 @@ if __name__ == "__main__":
     # Create an event loop to run the asynchronous function
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_messages())
-
-
