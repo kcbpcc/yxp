7d97e6ca8308d34ec891ff7bf1e556e26f037bfc
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 16:35:06 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -14,16 +14,8 @@ def send_message(bot, chat_id, text):
 
 def send_messages():
     # Read CSV file
-    csv_file_path = 'filePnL.csv'
+    csv_file_path = 'PnL.csv'
     df = pd.read_csv(csv_file_path)
-
-    # Check for headers in the CSV file
-    required_headers = {'qty', 'avg', 'close', 'ltp', 'open', 'high', 'low', 'PnL%_H', 'dPnL%', 'product', 'source', 'key', 'pxy', 'yxp', 'PnL%'}
-    missing_headers = required_headers - set(df.columns)
-
-    # If there are missing headers, handle it gracefully
-    if missing_headers:
-        print(f"Warning: Missing headers in CSV file: {missing_headers}")
 
     # Read telblock.txt to keep track of sent messages
     try:
@@ -31,9 +23,6 @@ def send_messages():
             telblock = set(map(str.strip, f.readlines()))
     except FileNotFoundError:
         telblock = set()
-
-    # Ensure that the DataFrame has the correct columns
-    df.columns = ['qty', 'avg', 'close', 'ltp', 'open', 'high', 'low', 'PnL%_H', 'dPnL%', 'product', 'source', 'key', 'pxy', 'yxp', 'PnL%'][:len(df.columns)]
 
     # Initialize the Telegram bot
     bot_instance = telegram.Bot(token=bot_token)
@@ -60,7 +49,3 @@ def send_messages():
 
 if __name__ == "__main__":
     send_messages()
-
-
-
-
