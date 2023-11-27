1a31d1d2af3157ad87e95322d83a3c9f5811587c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 11:00:04 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -1,66 +1,56 @@
-import os
-import telegram
-import asyncio
 import pandas as pd
+from telegram import Bot, ParseMode
+from telegram.error import Unauthorized
 
-# Constants
-BOT_TOKEN = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
-USER_USERNAMES = '-4022487175'  # Replace with your Telegram username or ID
+def send_messages(bot_token, user_usernames, csv_file_path='PnL.csv', telblock_file='telblock.txt'):
+    # Read CSV file
+    df = pd.read_csv(csv_file_path)
 
-def send_telegram_message(message_text, bot_token, user_usernames):
+    # Check for headers in the CSV file
+    required_headers = ['qty', 'avg', 'close', 'ltp', 'open', 'high', 'low', 'PnL%_H', 'dPnL%', 'product', 'source', 'key', 'pxy', 'yxp', 'PnL%']
+    missing_headers = set(required_headers) - set(df.columns)
+
+    # If there are missing headers, raise an exception or fix them programmatically
+    if missing_headers:
+        raise ValueError(f"Missing headers in CSV file: {missing_headers}")
+
+    # Read telblock.txt to keep track of sent messages
     try:
-        # Function to send a message to Telegram
-        async def send_message():
-            bot = telegram.Bot(token=bot_token)
-            await bot.send_message(chat_id=user_usernames, text=message_text)
-
-        # Send the message to Telegram
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(send_message())
-    except Exception as e:
-        # Handle the exception (e.g., log it) and continue with your code
-        print(f"Error sending message to Telegram: {e}")
-
-def process_new_rows(file_path, telblock_path, bot_token, user_usernames, header_list):
-    # Read telblock file to get the list of already processed keys
-    try:
-        with open(telblock_path, 'r') as telblock_file:
-            telblock = telblock_file.read().splitlines()
+        with open(telblock_file, 'r') as f:
+            telblock = set(map(str.strip, f.readlines()))
     except FileNotFoundError:
-        telblock = []
+        telblock = set()
 
-    # Read the CSV file without providing column names
-    try:
-        df = pd.read_csv(file_path, header=None)
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-        return
+    # Initialize the Telegram bot
+    bot = Bot(token=bot_token)
 
-    # Assign column names
-    df.columns = header_list
+    # Iterate through rows in the DataFrame
+    for _, row in df.iterrows():
+        # Construct message from the row data
+        message = "\n".join(f"{header}: {value}" for header, value in row.items())
 
-    # Iterate over rows
-    for index, row in df.iterrows():
-        # Assuming 'key' is the column containing key information
-        key_parts = str(row['key']).split(':')
-        
-        if key_parts[0] not in telblock:
-            message_text = f"{', '.join([f'{key.strip()}: {value.strip()}' for key, value in zip(key_parts[::2], key_parts[1::2])])} https://www.tradingview.com/chart/?symbol={key_parts[0]}"
-            send_telegram_message(message_text, bot_token, user_usernames)
-            # Append the processed key to telblock
-            with open(telblock_path, 'a') as telblock_file:
-                telblock_file.write(key_parts[0] + '\n')
+        # Check if the message has been sent before
+        if message in telblock:
+            continue
 
-# File paths
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-FILE_PATH = os.path.join(CURRENT_DIR, 'filePnL.csv')
-TELBLOCK_PATH = os.path.join(CURRENT_DIR, 'telblock.txt')
+        # Send the message to the specified user(s)
+        for username in user_usernames:
+            try:
+                bot.send_message(chat_id=username, text=message, parse_mode=ParseMode.MARKDOWN)
+            except Unauthorized:
+                print(f"Unauthorized: Could not send message to user {username}")
 
-# Provided header list
-HEADER_LIST = ['qty', 'avg', 'close', 'ltp', 'open', 'high', 'low', 'PnL%_H', 'dPnL%', 'product', 'source', 'key', 'pxy', 'yxp', 'PnL%']
+        # Record the sent message in telblock.txt to avoid double sending
+        telblock.add(message)
 
-# Execute the functionality automatically
-process_new_rows(FILE_PATH, TELBLOCK_PATH, BOT_TOKEN, USER_USERNAMES, HEADER_LIST)
+    # Update telblock.txt with the new messages
+    with open(telblock_file, 'w') as f:
+        f.write("\n".join(telblock))
+
+if __name__ == "__main__":
+    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
+    send_messages('6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo', ('-4022487175'))
+
 
 
 
