3d9792d51668d8ca8feaca7d016a8e5ffdd162ce
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 11:02:07 +0530

    Update telextpxy.py

--- sys/exe/run/telextpxy.py
+++ sys/exe/run/telextpxy.py
@@ -1,9 +1,22 @@
 import pandas as pd
-from telegram import Bot, ParseMode
-from telegram.error import Unauthorized
+import requests
 
-def send_messages(bot_token, user_usernames, csv_file_path='PnL.csv', telblock_file='telblock.txt'):
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
+user_usernames = ('-4022487175')
+
+def send_message(chat_id, text):
+    api_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    params = {'chat_id': chat_id, 'text': text, 'parse_mode': 'Markdown'}
+    response = requests.post(api_url, params=params)
+
+    if response.status_code != 200:
+        print(f"Failed to send message to {chat_id}. Status code: {response.status_code}")
+        print(response.text)
+
+def send_messages():
     # Read CSV file
+    csv_file_path = 'PnL.csv'
     df = pd.read_csv(csv_file_path)
 
     # Check for headers in the CSV file
@@ -16,13 +29,10 @@ def send_messages(bot_token, user_usernames, csv_file_path='PnL.csv', telblock_f
 
     # Read telblock.txt to keep track of sent messages
     try:
-        with open(telblock_file, 'r') as f:
+        with open('telblock.txt', 'r') as f:
             telblock = set(map(str.strip, f.readlines()))
     except FileNotFoundError:
         telblock = set()
-
-    # Initialize the Telegram bot
-    bot = Bot(token=bot_token)
 
     # Iterate through rows in the DataFrame
     for _, row in df.iterrows():
@@ -34,22 +44,19 @@ def send_messages(bot_token, user_usernames, csv_file_path='PnL.csv', telblock_f
             continue
 
         # Send the message to the specified user(s)
-        for username in user_usernames:
-            try:
-                bot.send_message(chat_id=username, text=message, parse_mode=ParseMode.MARKDOWN)
-            except Unauthorized:
-                print(f"Unauthorized: Could not send message to user {username}")
+        for chat_id in user_usernames:
+            send_message(chat_id, message)
 
         # Record the sent message in telblock.txt to avoid double sending
         telblock.add(message)
 
     # Update telblock.txt with the new messages
-    with open(telblock_file, 'w') as f:
+    with open('telblock.txt', 'w') as f:
         f.write("\n".join(telblock))
 
 if __name__ == "__main__":
-    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-    send_messages('6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo', ('-4022487175'))
+    send_messages()
+
 
 
 
