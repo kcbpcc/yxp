1c9361c7b26978b8f135cfc37360c042f2a17fcd
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 17:08:40 +0530

    Update 1pxy.py

--- sys/exe/run/1pxy.py
+++ sys/exe/run/1pxy.py
@@ -2,18 +2,23 @@ import telegram
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'
-user_usernames = ('-4022487175')
+user_usernames = ('-4022487175',)
 
 def send_message(bot, chat_id, text):
     bot.send_message(chat_id=chat_id, text=text)
 
-def send_hello():
+def send_messages():
     # Initialize the Telegram bot
     bot_instance = telegram.Bot(token=bot_token)
 
-    # Send the message to the specified user(s)
+    # Iterate through user IDs
     for chat_id in user_usernames:
-        send_message(bot_instance, chat_id, "Hello")
+        try:
+            # Send a simple test message
+            send_message(bot_instance, chat_id, "Test Message")
+            print(f"Sent message to chat_id: {chat_id}")
+        except Exception as e:
+            print(f"Error sending message to chat_id {chat_id}: {e}")
 
 if __name__ == "__main__":
-    send_hello()
+    send_messages()
