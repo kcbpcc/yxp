b6ac27d265149eb195a4e95c039836c222187f67
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 17:41:50 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -103,6 +103,28 @@ def mis_order_sell(index, row):
                 with open(csv_file_path, 'a', newline='') as csvfile:
                     csvwriter = csv.writer(csvfile)
                     csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                    try:
+                        import telegram
+                        import asyncio
+                        message_text = f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
+                        
+                        # Define the bot token and your Telegram username or ID
+                        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+                        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+                        
+                        # Function to send a message to Telegram
+                        async def send_telegram_message(message_text):
+                            bot = telegram.Bot(token=bot_token)
+                            await bot.send_message(chat_id=user_usernames, text=message_text)
+                    
+                    except Exception as e:
+                        # Handle the exception (e.g., log it) and continue with your code
+                        print(f"Error sending message to Telegram: {e}")
+                    
+                    # Send the 'row' content as a message to Telegram immediately after printing the row
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(send_telegram_message(message_text))
+                
                 return True                
             else:
                 logging.error("Order placement failed")
@@ -134,6 +156,28 @@ def mis_order_buy(index, row):
                 with open(csv_file_path, 'a', newline='') as csvfile:
                     csvwriter = csv.writer(csvfile)
                     csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                    try:
+                        import telegram
+                        import asyncio
+                        message_text = f"{str(row)} Symbol: {key} https://www.tradingview.com/chart/?symbol={key}"
+                        
+                        # Define the bot token and your Telegram username or ID
+                        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+                        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+                        
+                        # Function to send a message to Telegram
+                        async def send_telegram_message(message_text):
+                            bot = telegram.Bot(token=bot_token)
+                            await bot.send_message(chat_id=user_usernames, text=message_text)
+                    
+                    except Exception as e:
+                        # Handle the exception (e.g., log it) and continue with your code
+                        print(f"Error sending message to Telegram: {e}")
+                    
+                    # Send the 'row' content as a message to Telegram immediately after printing the row
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(send_telegram_message(message_text))
+                
                 return True                
             else:
                 logging.error("Order placement failed")
