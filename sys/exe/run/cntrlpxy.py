ef8bf81e00565c276dbcada088b62837c5f319ce
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-27 11:37:35 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -428,7 +428,7 @@ try:
                         row['qty'] > 0 and
                         row['source'] == 'holdings' and
                         row['product'] == 'CNC' and
-                        row['PnL%'] > 1.4 and
+                        row['PnL%'] < 1.4 and
                         ((row['PnL%'] < ((row['pxy'])) and row['PnL%_H'] > ((row['pxy']))) or (row['PnL%'] > TIMPXY))
                    
                     ):
